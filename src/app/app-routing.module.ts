dfbb4cd41a6cc8c4169f88c5832c8cbf3a97a0e2
SACHIN HS <sachin19927@gmail.com>
2023-04-18 00:38:19 +0530

    Routinng Push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LibHeaderComponent } from './component/library/lib-header/lib-header.component';
 import { ReceipeHeaderComponent } from './component/receipe/receipe-header/receipe-header.component';
+import { ListbooksComponent } from './component/library/listbooks/listbooks.component';
 
 const routes: Routes = [
-  {path:'book',
+  {path:'library',
   loadChildren:()=>import('./component/library/library.module').then(m=>m.LibraryModule)},
-  {path:'rec',
+  {path:'receipe',
   loadChildren:()=>import('./component/receipe/receipe.module').then(n=>n.ReceipeModule)},
-  {path:'library', component:LibHeaderComponent},
-  {path:'receipe', component:ReceipeHeaderComponent}
-  // {path:'listBooks', component:ListbooksComponent}
+
+  {path:'home',redirectTo:'library/listBooks',pathMatch:'full'},
+  {path:'',redirectTo:'library/listBooks',pathMatch:'full'},
 ];
 
 @NgModule({
