a8b4ab82f0a599703f610e9c3b1706f2784052ad
SACHIN HS <sachin19927@gmail.com>
2023-05-17 12:56:48 +0530

    reactive Form

--- src/app/component/library/addbookReactive/addbookReactive.component.ts
+++ src/app/component/library/addbookReactive/addbookReactive.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addbook',
   templateUrl: './addbookReactive.component.html',
   styleUrls: ['./addbookReactive.component.css']
 })
-export class AddbookReactiveComponent {
-form: any;
+export class AddbookReactiveComponent implements OnInit {
+
+myReactiveForm!: FormGroup;
+genders= ['Male','Female'];
+
+ngOnInit(): void {
+
+  this.myReactiveForm= new FormGroup(
+    {
+      userData:new FormGroup({
+
+      }),
+      title:new FormControl(null,Validators.required),
+      email:new FormControl(null,[Validators.required,Validators.email]),
+      gender: new FormControl('Male'),
+      comments: new FormControl(null),
+      category: new FormControl('Action',)
+  })
+
+  }
+
+  onSubmit()
+  {
+    console.log(this.myReactiveForm)
+  }
 
 }
