bf19efaf986d23a5e710d5e5cd846b44527a15fa
SACHIN HS <sachin19927@gmail.com>
2023-05-17 22:26:06 +0530

    REactive forms

--- src/app/component/library/addbookReactive/addbookReactive.component.ts
+++ src/app/component/library/addbookReactive/addbookReactive.component.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import {Observable} from 'rxjs';
 @Component({
   selector: 'app-addbook',
   templateUrl: './addbookReactive.component.html',
@@ -10,19 +10,30 @@ export class AddbookReactiveComponent implements OnInit {
 
 myReactiveForm!: FormGroup;
 genders= ['Male','Female'];
+forBiddenTitle= ['sachin','SACHIN','TEST'];
 
 ngOnInit(): void {
 
   this.myReactiveForm= new FormGroup(
     {
-      userData:new FormGroup({
-
+      metaData:new FormGroup({
+        comments: new FormControl(null,Validators.required),
+        category: new FormControl('Action',Validators.required)
       }),
-      title:new FormControl(null,Validators.required),
+      title:new FormControl(null,[Validators.required,this.forbidenName.bind(this)]),
       email:new FormControl(null,[Validators.required,Validators.email]),
       gender: new FormControl('Male'),
-      comments: new FormControl(null),
-      category: new FormControl('Action',)
+      hobbies: new FormArray([])
+
+  })
+
+
+  // this.myReactiveForm.valueChanges.subscribe(vaule=>{
+  //   console.log(vaule);
+  // })
+
+  this.myReactiveForm.statusChanges.subscribe(status=>{
+    console.log(status);
   })
 
   }
@@ -30,6 +41,66 @@ ngOnInit(): void {
   onSubmit()
   {
     console.log(this.myReactiveForm)
+    this.myReactiveForm.reset();
   }
+
+  onAddHobby(){
+    const control = new FormControl(null,Validators.required);
+        (<FormArray>this.myReactiveForm.get('hobbies')).push(control);
+
+  }
+
+  get myArrayControls(): AbstractControl[] {
+    return (this.myReactiveForm.get('hobbies') as FormArray).controls;
+  }
+
+  forbidenName(control:FormControl):{[s:string]:boolean} | null {
+    if(this.forBiddenTitle.indexOf(control.value)!==-1)
+    {
+      console.log('true')
+      return {'nameIsForbidden':true}
+    }
+     return null;
+    }
+
+
+    forbidenEmail(control:FormControl):Promise<any> | Observable<any>{
+      const promise = new Promise<any>((resolve,reject)=>{
+        setTimeout(()=>{
+          if(control.value==="sachin19927@gmail.com"){
+            resolve({'emailIsForbidden':true})
+          }
+          else
+            resolve(null)
+        },1500)
+      })
+      return promise
+    }
+
+
+    setValue()
+    {
+      this.myReactiveForm.setValue({
+        metaData:{
+          comments: 'TEST',
+          category: 'Horror'
+        },
+        title:'House',
+        email:'sachin19927@gmail.com',
+        gender: 'Male',
+        hobbies: []
+      })
+    }
+
+    patchValue()
+    {
+
+      this.myReactiveForm.patchValue({
+      title:'House of Dragon',
+      metaData:{
+      category: ''
+      }
+      });
+    }
 
 }
--- src/app/component/library/addbookTemplate/addbookTemplate.component.ts
+++ src/app/component/library/addbookTemplate/addbookTemplate.component.ts
@@ -8,62 +8,54 @@ import { NgForm, NgModel } from '@angular/forms';
 })
 export class AddbookTemplateComponent {
 
+  @ViewChild('myForm') libForm!:NgForm;
+  defaultSelect='Action';
+  bookComments='';
+  genders= ['Male','Female'];
 
-  @ViewChild('myForm') myForm! : NgForm;
-  @ViewChild('title') title! : NgModel;
-
-
-  defaultCategory= 'Action';
-  description='';
-  bookComments='TESTIG'
-
-  genders=['Male','Female']
-
-  bookData ={
-
-      "title":"",
-      "email":"",
-    "category":"",
-    "comments":"",
-    "gender":""
+  library ={
+    title: '',
+    email:'',
+    category:'',
+    comments:'',
+    gender:''
   }
 
-
-  SetInitialBook(){
-    this.myForm.setValue({
-      libData:{
-        title:'House of dragon',
-        email:'sachin@gmail.com',
-        category:'Horror'
-      }
-      ,
-      gender:'Male',
-      comments:'TESTING'
-    })
-  };
-
-  PatchInitialBook(){
-    this.myForm.form.patchValue({
-      gender:'Female',
-      comments:'DEMO'
-    })
-  }
-
-
-  onSubmit()
-  {
-    console.log(this.myForm);
-    this.bookData.title=this.myForm.value.libData.title;
-    this.bookData.category=this.myForm.value.libData.category;
-    this.bookData.email=this.myForm.value.libData.email;
-    this.bookData.comments=this.myForm.value.comments;
-    this.bookData.gender=this.myForm.value.gender;
-
-    this.myForm.reset();
-
-  }
-
-
+onSubmitLocalElementRef(form: ElementRef){
+ console.log(form)
+}
+onSubmitNgForm(form: NgForm){
+  console.log(form)
 }
 
+onSubmit(){
 
+  console.log(this.libForm)
+  this.library.title=this.libForm.value.libData.title;
+  this.library.email=this.libForm.value.libData.email;
+  this.library.category=this.libForm.value.libData.category;
+  this.library.comments=this.libForm.value.libData.comments;
+  this.library.gender=this.libForm.value.libData.gender;
+
+  this.libForm.reset();
+}
+
+suggestBook(){
+   this.libForm.setValue({
+     libData:{
+       title:'House of Dragon',
+       email:'sachin.hs@gmail.com',
+       category:'Action',
+       comments:'',
+       gender:'Male'
+     }
+   })
+  }
+  patchBook(){
+  this.libForm.form.patchValue({
+    libData:{
+      title:'House of Dragon'
+    }
+  })
+  }
+}
